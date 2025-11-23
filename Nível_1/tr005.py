b52877406d517dc2b631e7646260f4e999ed239c
Felipe Alencar Lima <felipe.alencarlima@gmail.com>
2025-11-22 21:29:53 -0300

    Refactor input and loop formatting in tr005.py

--- Nível_1/tr005.py
+++ Nível_1/tr005.py
@@ -1,4 +1,4 @@
-numcalc = int(input("Digite um numero: "))
-for c in range(1,11):
+numcalc = int(input("Digite um numero: ")) # ler um número 
+for c in range(1,11): 
     valor = numcalc * c
     print(f'{numcalc} x {c} = {valor}')
