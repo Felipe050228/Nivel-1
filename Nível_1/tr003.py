6fbaa305b597674015ac8b7a118d5e13919c23aa
Felipe Alencar Lima <felipe.alencarlima@gmail.com>
2025-11-22 21:23:06 -0300

    Add comments and format output in tr003.py
    Refactor code to add comments and improve output format.

--- Nível_1/tr003.py
+++ Nível_1/tr003.py
@@ -1,5 +1,4 @@
-num1 = int(input("digite um numero: "))
-num2 = int(input("ditite outro numero: "))
-soma = num1 + num2
-lim = '---'*2
-print(f'   {num1} \n + {num2} \n{lim} \n   {soma}')
+num1 = int(input("digite um numero: ")) # ler o primeiro número 
+num2 = int(input("ditite outro numero: ")) # ler o segundo número 
+soma = num1 + num2 # guarda a soma na variável "soma"
+print(f'{num1}+{num2}={soma}') # monstra o resultado junto com o primeiro e o segundo número 
